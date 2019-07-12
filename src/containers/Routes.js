34b2794db53a5e8e5890234572edf7d3416d98e0
su_ozbek <suleyman@moobi.com.tr>
2019-07-12 10:46:52 +0300

    Component added

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import RoutesTable from '../components/RoutesTable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { faMap, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { DatePicker, Button } from 'antd';
 class Routes extends Component {
-    
-    render(){
-     return(
-         <div>
-             <RoutesTable />
-         </div>
-     );   
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <FontAwesomeIcon className="menuIcon" icon={faMap} />
+          <DatePicker />
+          <Button>
+            <FontAwesomeIcon className="menuIcon" icon={faArrowRight} />
+          </Button>
+          <Button type="primary" shape="circle" icon="plus" size="large" />
+        </div>
+        <h3>Routes</h3>
+        <p>Create Report</p>
+        <RoutesTable />
+      </div>
+    );
+  }
 }
 
 export default Routes;
